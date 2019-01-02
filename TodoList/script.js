81451042562bbe0f29d74608fedfb901d9317676
Yujin <yujin.kim@rea-group.com>
2019-01-02 15:57:10 +1100

    add checkbox & remove button

--- TodoList/script.js
+++ TodoList/script.js
@@ -2,18 +2,22 @@
 (function() {
     var state = [
         {
+            id: 1,
             done: false,
             content: 'sing a song',
         },
         {
+            id: 2,
             done: false,
             content: 'study javascript',
         },
         {
+            id: 3,
             done: true,
             content: 'drink coffee',
         },
         {
+            id: 4,
             done: false,
             content: 'farewell Bob at lawn bowls',
         },
@@ -21,6 +25,8 @@
     var rootElement = document.getElementById('root');
     var formElement = document.getElementById('form');
     var inputElement = document.getElementById('addTodoInput');
+    var checkboxStatus = document.getElementsByTagName('INPUT');
+    var nextId = 999;
 
     // functions that return values
 
@@ -29,32 +35,78 @@
     function handleSubmit(event) {
         event.preventDefault();
         var newTodo = {
+            id: nextId,
             done: false,
             content: inputElement.value,
         }
+        nextId++;
         state.push(newTodo);
-        rootElement.innerHTML = '';
 
+        render();
+
+    }
+    
+    function renderTodo(todo) {
+        var listElement = document.createElement('li');
+        var checkboxElement = document.createElement('INPUT');
+        checkboxElement.id = todo.id;
+        checkboxElement.type = "checkbox";
+        checkboxElement.checked = todo.done; 
+
+        var labelElement = document.createElement('label');
+        labelElement.htmlFor = todo.id;
+        labelElement.innerText = todo.content;
+
+        var deleteButton = document.createElement('button');
+        deleteButton.type = 'button';
+        deleteButton.innerText = 'x';
+        deleteButton.addEventListener('click', handleDeleteTodo);
+        
+        rootElement.appendChild(listElement);
+        listElement.appendChild(checkboxElement);
+        listElement.appendChild(labelElement);
+        listElement.appendChild(deleteButton);
+
+        
+    }
+    function handleDeleteTodo(event) {
+        event.preventDefault();
+        var listElement = event.target.parentElement;
+        // rootElement.removeChild(listElement);
+        var labelElement = listElement.querySelector('label');
+        var todoId = Number(labelElement.htmlFor);
+        function findTodoById(item){
+            return item.id === todoId;
+        }
+        var todoIndex = state.findIndex(findTodoById);
+        state.splice(todoIndex,1)
         render();
     }
 
-    
-    function addTodo(content) {
-        var listElement = document.createElement('li');
-        listElement.innerHTML = content;
-        rootElement.appendChild(listElement);
-    }
-    
     function render() {
+        rootElement.innerHTML = '';
         state.forEach(function(todo){
-            addTodo(todo.content);
-  
-        })
+            renderTodo(todo);     
+        })       
     };
 
     render();
 
+
+    // function testfunction(){
+    //     // var test = document.querySelectorAll("li");
+    //     // var test1 = test[1].getAttribute("checked");
+    //     console.log('hey');
+            
+    // }
+
+    // testfunction();
+
+    // document.getElementById('todoStatus').addEventListener('click', changeTodoStatus);
     formElement.addEventListener('submit', handleSubmit);
+
+    // document.getElementById('woo').addEventListener('click', testfunction)
+
 
 })();
 
@@ -62,9 +114,8 @@
 
 /** 
  * TODO: 
- * Add delete button
+ * ---Add delete button
  * styles
- * checkboxs (done/not done)
  * striketrhough for done items
  * write tests
  * */ 
