aeb41e24d029677e87af6bc1a8aa67fc9ff39a02
Yujin <yujin.kim@rea-group.com>
2019-02-06 16:40:29 +1100

    add filterTodos test and WIP

--- script.js
+++ script.js
@@ -1,157 +1,203 @@
-
 var yujinsSweetSweetTodoList = (function() {
-    var count = 0;
-    var state = [
-        {
-            id: 1,
-            done: false,
-            content: 'sing a song',
-        },
-        {
-            id: 2,
-            done: false,
-            content: 'study javascript',
-        },
-        {
-            id: 3,
-            done: true,
-            content: 'drink coffee',
-        },
-        {
-            id: 4,
-            done: false,
-            content: 'farewell Bob at lawn bowls',
-        },
-    ];
-    var rootElement = document.getElementById('root');
-    var formElement = document.getElementById('form');
-    var inputElement = document.getElementById('addTodoInput');
-    var completedBtn = document.getElementById('completedBtn');
-    var nextId = 999;
+  var count = 0;
+  var NONE = "NONE";
+  var COMPLETED = "COMPLETED";
+  var ACTIVE = "ACTIVE";
+  var REMOVED = "REMOVED";
+  var state = {
+    filter: NONE,
+    todos: [
+      {
+        id: 1,
+        done: false,
+        content: "sing a song",
+        delete: false
+      },
+      {
+        id: 2,
+        done: true,
+        content: "study javascript",
+        delete: false
+      },
+      {
+        id: 3,
+        done: true,
+        content: "drink coffee",
+        delete: false
+      },
+      {
+        id: 4,
+        done: false,
+        content: "GOODBYE Media :((((((",
+        delete: false
+      }
+    ]
+  };
+  var rootElement = document.getElementById("root");
+  var formElement = document.getElementById("form");
+  var inputElement = document.getElementById("addTodoInput");
+  var completedBtn = document.getElementById("completedBtn");
+  var allBtn = document.getElementById("allBtn");
+  var activeBtn = document.getElementById("activeBtn");
+  var removedBtn = document.getElementById("removedBtn");
+  var nextId = 999;
 
-    // functions that return values
-    function createCheckboxElement(todo){
-        var checkboxElement = document.createElement('INPUT');
-        checkboxElement.id = todo.id;
-        checkboxElement.type = "checkbox";
-        checkboxElement.checked = todo.done; 
-        checkboxElement.addEventListener('change', handleDoneTodo)
-        return checkboxElement;
+  // functions that return values
+  function createCheckboxElement(todo) {
+    var checkboxElement = document.createElement("INPUT");
+    checkboxElement.id = todo.id;
+    checkboxElement.type = "checkbox";
+    checkboxElement.checked = todo.done;
+    checkboxElement.addEventListener("change", toggleTodo);
+    return checkboxElement;
+  }
+
+  function createLabelElement(todo) {
+    var labelElement = document.createElement("label");
+    labelElement.htmlFor = todo.id;
+    labelElement.innerText = todo.content;
+    if (todo.done === true) {
+      var str = labelElement.innerText;
+      var result = str.strike();
+      labelElement.innerHTML = result;
     }
+    return labelElement;
+  }
 
-    function createLabelElement(todo) {
-        var labelElement = document.createElement('label');
-        labelElement.htmlFor = todo.id;
-        labelElement.innerText = todo.content;
-        if (todo.done === true){
-            var str = labelElement.innerText;
-            var result = str.strike(); 
-            labelElement.innerHTML=result;
-        }
-        return labelElement;
-    }
+  function createDeleteButton(eventHandler) {
+    var deleteButton = document.createElement("button");
+    deleteButton.type = "button";
+    deleteButton.innerText = "x";
+    deleteButton.addEventListener("click", eventHandler);
+    return deleteButton;
+  }
 
-    function createDeleteButton(eventHandler) {
-        var deleteButton = document.createElement('button');
-        deleteButton.type = 'button';
-        deleteButton.innerText = 'x';
-        deleteButton.addEventListener('click', eventHandler);
-        return deleteButton;
-    }
+  function createListElement(todo) {
+    var listElement = document.createElement("li");
+    var checkboxElement = createCheckboxElement(todo);
+    var labelElement = createLabelElement(todo);
+    var deleteButton = createDeleteButton(handleDeleteTodo);
 
-    function createListElement(todo) {
-        var listElement = document.createElement('li');
-        var checkboxElement = createCheckboxElement(todo);
-        var labelElement = createLabelElement(todo);
-        var deleteButton = createDeleteButton(handleDeleteTodo);
-        
-        // DOM stuff
-        listElement.appendChild(checkboxElement);
-        listElement.appendChild(labelElement);
-        listElement.appendChild(deleteButton);
-        return listElement; 
-    }
+    // DOM stuff
+    listElement.appendChild(checkboxElement);
+    listElement.appendChild(labelElement);
+    listElement.appendChild(deleteButton);
+    return listElement;
+  }
 
-    // functions that have side effects
+  // functions that have side effects
 
-    function handleSubmit(event) {
-        count++;
-        console.log(`handleSubmit called ${count} times`);
-        event.preventDefault();
-        var newTodo = {
-            id: nextId,
-            done: false,
-            content: inputElement.value,
-        }
-        nextId++;
-        state.push(newTodo);
-
-        render(state);
-
-        formElement.reset();
-    }
-    
-
-    function handleDoneTodo(event) {
-        console.log(event.target.id, typeof event.target.id );
-        var labelElementId = Number(event.target.id);
-        state.forEach(function(todo) {
-            if (todo.id === labelElementId) {
-                todo.done = !todo.done;
-            }
-        });
-        
-        render(state);
-    }
-
-    function handleDeleteTodo(event) {
-        event.preventDefault();
-        var listElement = event.target.parentElement;
-        var labelElement = listElement.querySelector('label');
-        var todoId = Number(labelElement.htmlFor);
-        function findTodoById(item){
-            return item.id === todoId;
-        }
-        var todoIndex = state.findIndex(findTodoById);
-        state.splice(todoIndex,1)
-        render(state);
-    }
-
-    function filterCompletedTodo() {
-        var filteredTodos = state.filter(function(todo) {
-            if(todo.done) {
-                return todo
-            }
-        });
-        render(filteredTodos);
-    }
-
-    function render(todos) {
-        rootElement.innerHTML = '';
-        todos.forEach(function(todo){    
-            rootElement.appendChild(createListElement(todo));
-        })       
+  function handleSubmit(event) {
+    count++;
+    console.log(`handleSubmit called ${count} times`);
+    event.preventDefault();
+    var newTodo = {
+      id: nextId,
+      done: false,
+      content: inputElement.value,
+      delete: false
     };
+    nextId++;
+    state.todos.push(newTodo);
+    console.log(state);
 
     render(state);
 
-    formElement.addEventListener('submit', handleSubmit);
-    completedBtn.addEventListener('click', filterCompletedTodo)
+    formElement.reset();
+  }
 
-    return {
-        createCheckboxElement: createCheckboxElement,
-        createLabelElement: createLabelElement,
-        createDeleteButton: createDeleteButton,
-        createListElement: createListElement,
+  function toggleTodo(event) {
+    var labelElementId = Number(event.target.id);
+    state.todos.forEach(function(todo) {
+      if (todo.id === labelElementId) {
+        todo.done = !todo.done;
+      }
+    });
+
+    render(state);
+  }
+
+  function handleDeleteTodo(event) {
+    event.preventDefault();
+    var listElement = event.target.parentElement;
+    var labelElement = listElement.querySelector("label");
+    var todoId = Number(labelElement.htmlFor);
+
+    function findDeleteTodo(item) {
+      return item.id === todoId;
     }
 
+    state.todos.find(findDeleteTodo).delete = true;
+
+    function findTodoById(item) {
+      return item.id === todoId;
+    }
+
+    var todoIndex = state.todos.findIndex(findTodoById);
+    state.todos.splice(todoIndex, 1);
+    render(state);
+  }
+
+  function filterTodos(state) {
+    return state.todos.filter(function(todo) {
+      if (state.filter === COMPLETED && todo.done) {
+        return todo;
+      }
+      if (state.filter === ACTIVE && !todo.done) {
+        return todo;
+      }
+      if (state.filter === NONE) {
+        return todo;
+      }
+      if (state.filter === REMOVED && todo.delete) {
+        return todo;
+      }
+    });
+  }
+
+  function render(state) {
+    rootElement.innerHTML = "";
+    const filteredTodos = filterTodos(state);
+    filteredTodos.forEach(function(todo) {
+      rootElement.appendChild(createListElement(todo));
+    });
+  }
+
+  function handleFilterClick(event) {
+    if (event.srcElement.id === "completedBtn") {
+      state.filter = COMPLETED;
+    } else if (event.srcElement.id === "activeBtn") {
+      state.filter = ACTIVE;
+    } else if (event.srcElement.id === "deleteBtn") {
+      state.filter = DONE;
+    } else if (event.srcElement.id === "removedBtn") {
+      state.filter = REMOVED;
+    } else {
+      state.filter = NONE;
+    }
+    render(state);
+  }
+
+  render(state);
+
+  formElement.addEventListener("submit", handleSubmit);
+  completedBtn.addEventListener("click", handleFilterClick);
+  allBtn.addEventListener("click", handleFilterClick);
+  activeBtn.addEventListener("click", handleFilterClick);
+  removedBtn.addEventListener("click", handleFilterClick);
+
+  return {
+    createCheckboxElement: createCheckboxElement,
+    createLabelElement: createLabelElement,
+    createDeleteButton: createDeleteButton,
+    createListElement: createListElement,
+    filterTodos: filterTodos
+  };
 })();
 
-
-
-/** 
- * TODO: 
+/**
+ * TODO:
  * styles
  * write tests
- * add filters
- * */ 
+ * add removed filter
+ * */
--- test.js
+++ test.js
@@ -1,27 +1,54 @@
 function expect(left) {
-    return {
-        toEqual: function(right) {
-            if (JSON.stringify(left) !== JSON.stringify(right)) {
-                throw new Error('Test failed\n'
-                + 'Expected: ' + JSON.stringify(right) + '\n'
-                + 'Received: ' + JSON.stringify(left) + '\n'
-                )
-            }
-        },
+  return {
+    toEqual: function(right) {
+      if (JSON.stringify(left) !== JSON.stringify(right)) {
+        throw new Error(
+          "Test failed\n" +
+            "Expected: " +
+            JSON.stringify(right) +
+            "\n" +
+            "Received: " +
+            JSON.stringify(left) +
+            "\n"
+        );
+      }
     }
+  };
 }
 
 (function() {
-    var delButton = yujinsSweetSweetTodoList.createDeleteButton(function(){});
-    expect(delButton.type).toEqual('button')
-    console.log('createDeleteButton() is a button element')
+  var delButton = yujinsSweetSweetTodoList.createDeleteButton(function() {});
+  expect(delButton.type).toEqual("button");
+  console.log("createDeleteButton() is a button element");
 })();
+
+// (function() {
+//   var delButton = yujinsSweetSweetTodoList.createDeleteButton(function() {});
+//   // make this test pass
+//   expect(true).toEqual(true);
+//   console.log("createDeleteButton() should be a button element");
+// })();
 
 (function() {
-    var delButton = yujinsSweetSweetTodoList.createDeleteButton(function(){});
-    isTrue('createDeleteButton() should be a button element', delButton.type === 'button');
+  let testState = {
+    filter: "NONE",
+    todos: [
+      {
+        id: 1,
+        done: false,
+        content: "sing a song",
+        delete: false
+      },
+      {
+        id: 2,
+        done: true,
+        content: "study javascript",
+        delete: false
+      }
+    ]
+  };
+  var filterTodos = yujinsSweetSweetTodoList.filterTodos(testState);
+  // make this test pass
+  expect(Array.isArray(filterTodos)).toEqual(true);
+  console.log("filterTodos() should return an array of filtered todos");
 })();
-
-
-
-
